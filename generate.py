9d0281975f04c63084c43ef83f398146cbfdac1e
Peter Downs <peter.l.downs@gmail.com>
2012-01-08 16:19:33 -0500

    Fixes syntax error

--- generate.py
+++ generate.py
@@ -9,19 +9,23 @@ import markdown
 # Build the JSON dict of posts and HTML
 posts = {}
 for filepath in sys.argv[1:]:
+	print filepath
 	filename = filepath.split(os.sep)[-1]
+	print filename
 	title = filename.split('.md')[0]
+	print title
+	print ""
 	try:
-		with open(filename) as fin:
+		with open(filepath) as fin:
 			md_content = fin.read()
 	except Exception as e:
 		print e
-		md_content = None
+		md_content = ""
 	post = {
-		"path" : filename
+		"path" : filename,
 		"html" : markdown.markdown(md_content)
 	}
-	posts['title'] = post
+	posts[title] = post
 
 # Write it to file
 with open('posts.json', 'w') as fout:
