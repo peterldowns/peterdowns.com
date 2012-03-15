2351ec55fc2862f89615e928d4f5d47a2e841936
Peter Downs <peter.l.downs@gmail.com>
2012-03-14 22:40:43 -0400

    Fixes typos

--- generate.py
+++ generate.py
@@ -3,6 +3,7 @@ import json
 import markdown
 from operator import itemgetter
 from bottle import template, view
+from time import mktime, strptime
 
 _MDFolder = "./md"
 _HTMLFolder = "./posts"
@@ -18,7 +19,7 @@ def renderHomepage(posts):
 
 @view('index.tpl')
 def renderPost(post):
-	tempateData = {
+	templateData = {
 		"post" : post,
 		"view" : "post"
 	}
@@ -36,34 +37,38 @@ def parseMD(fstr):
 	html = _Parser.convert(fstr)
 	meta = _Parser.Meta
 	meta['html'] = html
+	
+	meta['date'] = meta['date'][0]
+	meta['title'] = meta['title'][0]
+	meta['author'] = meta['author'][0]
+
 	return meta
 
 def makePost(path):
 	""" Given a path, return a post dictionary. """
 	path = os.path.abspath(path)
-	raw_md = readMDFile(path)
+	raw_md = readMD(path)
 	post = parseMD(raw_md)
 	
 	post['MDpath'] = path
 	post['MDfile'] = filename = path.rsplit(os.sep, 1)[1]
-	post['MDtitle'] = filename.rsplit['.', 1)[0]
+	post['MDtitle'] = filename.rsplit('.', 1)[0]
 
 	time_format = "%A, %B %d, %Y"
-	timestamp = mktime(strptime(post['date']))
+	timestamp = mktime(strptime(post['date'], time_format))
 	post.update({
-		"HTMLpath" : "{}/{}.html".format(_HTMLFolder, post['filetitle'])
+		"HTMLpath" : "{}/{}.html".format(_HTMLFolder, post['MDtitle']),
 		"timestamp" : timestamp,
 	})
 	return post
 
 def buildPosts(in_folder):
-	folder_path = os.path.abspath(folder_path)
-	filepaths = map(lambda f: os.sep.join((folder_path, f)), os.listdir(folder_path))
+	in_folder = os.path.abspath(in_folder)
+	filepaths = map(lambda f: os.sep.join((in_folder, f)), os.listdir(in_folder))
 
 	posts_out = []
 	for post_path in filepaths:
 		post = makePost(post_path)
-		post = preparePost(post)
 		posts_out.append(post)
 	return posts_out
 
@@ -76,14 +81,14 @@ if __name__=="__main__":
 
 	print "Writing homepage -> ./index.html"
 	with open('./index.html', 'w') as fout:
-		homepageHTML = renderHompeage(posts)
+		homepageHTML = renderHomepage(posts)
 		fout.write(homepageHTML)
 	
 	cur_ind = 0
 	num_posts = len(posts)
 	print "Writing {} posts:".format(num_posts)
 	for post in posts:
-		print "\t[{} / {}] {} -> {}".format(cur_ind+1, num_posts, post['title'], post['HTMLpath'])
+		print "\t[{} / {}] {} -> {}".format(cur_ind+1, num_posts, post['MDpath'], post['HTMLpath'])
 		cur_ind += 1
 		with open(post['HTMLpath'], 'w') as fout:
 			postHTML = renderPost(post)
