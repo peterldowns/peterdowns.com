0c3b1b7f2492fba144997df5a69026d1c0606146
Peter Downs <peterldowns@gmail.com>
2014-01-22 01:00:00 -0800

    Stop attempting to write analytics file.

--- generate.py
+++ generate.py
@@ -88,10 +88,10 @@ if __name__=='__main__':
   try: os.mkdir(_md_out) # make the output folder
   except OSError: pass # already exists
 
-  print 'Writing running -> {}'.format(_running_out)
-  with open(_running_out, 'w') as fout:
-    html = render_running_analytics(_running_yaml_path)
-    fout.write(unicode(html).encode(errors=_enc_errors))
+  #print 'Writing running -> {}'.format(_running_out)
+  #with open(_running_out, 'w') as fout:
+  #  html = render_running_analytics(_running_yaml_path)
+  #  fout.write(unicode(html).encode(errors=_enc_errors))
 
   print 'Writing homepage -> {}'.format(_index)
   with open(_index, 'w') as fout:
